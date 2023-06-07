5ed8e99006147eee1b10112c1f7e7fad042b5a3c
EshaanSandhu <110472900+EshaanSandhu@users.noreply.github.com>
2023-06-07 07:22:40 +0530

    Add files via upload

---
+++ ques3.java
@@ -0,0 +1,12 @@
+class ques3{
+public static void main(String args[]){
+float a=24;
+float b=30;
+float c=60;
+float d=a+b+c;
+int inint=(int) d;
+double indouble=(double) d;
+System.out.println("Sum is"+d);
+System.out.println("In int"+inint);
+System.out.println("In double"+indouble);
+}}
